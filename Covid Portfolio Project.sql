908213249d2c06d09c163844f4e9c3cf4f21a759
ShongweNE <127794292+ShongweNE@users.noreply.github.com>
2023-03-13 23:05:48 +0200

    Update Covid Portfolio Project.sql

--- Covid Portfolio Project.sql
+++ Covid Portfolio Project.sql
@@ -1,18 +1,23 @@
+/*
+Covid 19 Data Exploration 
+Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+*/
+
+
 USE PortfolioProject
 SELECT *
 FROM PortfolioProject.dbo.CovidDeaths
 WHERE continent is not null
 ORDER BY 3,4
 
---SELECT *
---FROM PortfolioProject.dbo.CovidVaccinations
---ORDER BY 3,4
+
+-- Select Data that we are going to be starting with
 
 SELECT location, date, total_cases, new_cases, total_deaths, population
 FROM PortfolioProject.dbo.CovidDeaths
 ORDER BY location, date
 
---Looking at Total Cases vd Total Deaths
+Total Cases vs Total Deaths
 -- Shows the likelihood of dying if you contract covid in your country
 
 SELECT location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
@@ -22,7 +27,7 @@ ORDER BY location, date
 
 
 --Looking at Total Cases vs Population
--- Shows what percentage of population got Covid
+-- Shows what percentage of population infected with Covid
 
 SELECT location, date, total_cases, population, (total_cases/population)*100 as DeathPercentage  
 FROM PortfolioProject.dbo.CovidDeaths
@@ -30,14 +35,14 @@ WHERE location = 'South Africa'
 ORDER BY location, date
 
 
---Highest Number of Infections at peak of infections as a percentage
+--Countries with Highest Infection RAte compared to Population
 
 SELECT location, date, total_cases, population, (total_cases/population)*100 as PercentPopulationInfected 
 FROM PortfolioProject.dbo.CovidDeaths
 WHERE location = 'South Africa'
 ORDER BY PercentPopulationInfected DESC
 
---Looking at COuntries with Highest Infection Rate Compared to Population
+-- Countries with Highest Infection Rate Compared to Population
 
 SELECT location, population, MAX(total_cases/population) as HighestInfectionCount,
 MAX((total_cases/population))*100 as PercentPopulationInfected
@@ -45,7 +50,7 @@ FROM PortfolioProject.dbo.CovidDeaths
 GROUP BY location, population
 ORDER BY PercentPopulationInfected DESC
 
---Showing Countries with Highest Death Count per Population
+-- Countries with Highest Death Count per Population
 
 SELECT location, MAX(CAST(total_deaths AS INT)) as TotalDeathCount
 FROM PortfolioProject.dbo.CovidDeaths
@@ -61,13 +66,7 @@ WHERE continent is not null
 GROUP BY continent
 ORDER BY TotalDeathCount DESC
 
-/* Double check why im getting more returns not only just continents
-SELECT location, MAX(CAST(total_deaths AS INT)) as TotalDeathCount
-FROM PortfolioProject.dbo.CovidDeaths
-WHERE continent is null
-GROUP BY location
-ORDER BY TotalDeathCount DESC*/
-
+--BREAK DOWN BY CONTINENT
 
 --Showing continents with highest death count per population
 
@@ -87,9 +86,10 @@ FROM PortfolioProject.dbo.CovidDeaths
 WHERE continent is not null
 ORDER BY 1,2
 
+
+
 -- Total Populatoin vs Vaccinations
 -- Shows Percentage of Population that has recieved at least one Covid Vaccine
-
 
 SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
  SUM(CONVERT(bigint,vac.new_vaccinations)) 
@@ -100,8 +100,6 @@ JOIN  PortfolioProject.dbo.CovidVaccinations vac
 	AND dea.date = vac.date
 WHERE dea.continent is not null
 ORDER BY  2,3
-
-
 
 -- Using CTE to perform Calculationon Partition By in previous query
 
@@ -145,7 +143,6 @@ SELECT *, (RollingPeopleVaccinated/Population) * 100
 FROM  #PercentagePopulationVaccinated
 
 
-
 --Creating view to store data for later visualizations
 
 CREATE VIEW PercentPopulationVaccinated as 
@@ -157,5 +154,3 @@ JOIN  PortfolioProject.dbo.CovidVaccinations vac
 	AND dea.date = vac.date
 WHERE dea.continent is not null
 
-SELECT *
-FROM PercentPopulationVaccinated
